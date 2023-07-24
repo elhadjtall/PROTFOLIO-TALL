9c0e8bd2b00878bd45272a4fcd5f76fec5dc3f80
unknown <elhadjoumartall10@gmail.com>
2023-07-24 13:06:01 +0200

    un modal en js

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ btn.onclick = function () {
 span.onclick = function () {
     modal.style.display = "none";
 }
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
+}
